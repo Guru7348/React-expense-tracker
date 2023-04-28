11b761484c0e7a18277678af87b168becd03f887
Guru <gurup4627@gmail.com>
2023-04-28 14:15:21 +0530

    changed title

--- src/context/contextprovider.js
+++ src/context/contextprovider.js
@@ -4,23 +4,7 @@ import reducer from "./reducer";
 export const Datacontext = createContext(null)
 export default function Data({children}){
 
-    const datalist = [
-        {
-            id:0,
-            name:'recharge',
-            price:-200
-        },
-        {
-            id:1,
-            name:'refund',
-            price:+5000
-        },
-        {
-            id:2,
-            name:'earphone',
-            price:-2500
-        }
-    ]
+    const datalist = []
 
     const [state,dispatch] = useReducer(reducer,datalist)
 
